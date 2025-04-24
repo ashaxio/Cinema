bd62f165861a6e7fde8c242eda24421d20f52204
Oleksandr Bezverkhyi <bezverkhyi.ol@gmail.com>
2025-04-24 20:08:19 +0300

    Set up React routing and create basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
+import FavoritesPage from "./pages/FavoritesPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminMovies from "./pages/AdminMovies";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>bebraaaaaaa</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/movies/:id" element={<MoviePage />}></Route>
+      <Route path="/favorites" element={<FavoritesPage />}></Route>
+      <Route path="/admin" element={<AdminDashboard />}></Route>
+      <Route path="/admin/movies" element={<AdminMovies />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
