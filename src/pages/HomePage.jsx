c31b7b5155482910468ab748da1dac22d909fea5
wthellomg <9818138@gmail.com>
2025-04-29 14:58:00 +0300

    create MovieCard -added MovieCard.jsx -changed HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import Navbar from "../components/navbar";
+import MovieCard from "../components/MovieCard";
 
 const HomePage = () => {
   return (
     <>
       <Navbar userId="">
-        <h1>Home Page!</h1>
+        <div class="flex flex-wrap justify-between gap-6 p-6">
+        <MovieCard id={1} />
+        <MovieCard id={2} />
+        <MovieCard id={3} />
+        <MovieCard id={4} />
+        <MovieCard id={5} />
+        <MovieCard id={6} />
+        <MovieCard id={7} />
+        <MovieCard id={8} />
+        <MovieCard id={9} />
+        <MovieCard id={10} />
+        </div>
       </Navbar>
     </>
   );
