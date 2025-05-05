59aef8e7e31e01832485030c21c9860a9cfcfc84
Andriy Murhan <andrijmurgan@gmail.com>
2025-05-05 22:54:28 +0300

    Added 'Choose sessions' button

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -3,8 +3,9 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { FilmDataContext } from '../FilmDataProvider';
 import { AuthContext } from '../components/AuthContext';
 import { formatDate } from '../utils/DateUtils';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/navbar';
-import NotFound from "./NotFound";
+import NotFound from './NotFound';
 import PersonCard from '../components/PersonCard';
 
 import searchIcon from '../assets/search.svg';
@@ -22,9 +23,7 @@ const MoviePage = () => {
   const sliderRef = useRef();
 
   useEffect(() => {
-    const foundMovie = films.find(
-      (film) => String(film.id) === String(movieId)
-    );
+    const foundMovie = films.find((film) => film.id === parseInt(movieId));
     if (foundMovie) setMovie(foundMovie);
   }, [films, movieId]);
 
@@ -34,7 +33,7 @@ const MoviePage = () => {
 
     const ratingData = {
       id: movieId,
-      user: user.username || "Not specified",
+      user: user.username || 'Not specified',
       rating: rating,
     };
 
@@ -55,12 +54,37 @@ const MoviePage = () => {
     });
   };
 
-  if (loading) return <Navbar><h1>Loading...</h1></Navbar>;
-  if (!movie) return <NotFound/>;
-  
+  if (loading)
+    return (
+      <Navbar>
+        <h1>Loading...</h1>
+      </Navbar>
+    );
+  if (!movie) return <NotFound />;
+
   return (
     <Navbar>
-      <div className='movie-content -m-6'>
+      <div className='movie-content -m-6 relative'>
+        {/* Buy ticket button */}
+        <Link
+          to='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'
+          className='fixed top-[89vh] left-[83vw] flex justify-center items-center gap-x-3 bg-[#CB134A] h-17 w-56 rounded-full hover:cursor-pointer hover:bg-[#9D5F64] transition-all'
+        >
+          <svg
+            viewBox='0 0 34 34'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            class='lg:h-8 lg:w-8 w-6 h-6 mt-1'
+          >
+            <path
+              d='M22.5166 25.9836L21.051 24.5181C20.2843 23.7513 19.0412 23.7513 18.2744 24.5181C17.5077 25.2848 17.5077 26.5279 18.2744 27.2947L19.74 28.7602L16.5005 31.9997C15.0522 33.448 12.7041 33.448 11.2558 31.9997L9.71324 30.4572C9.55114 30.2951 9.50569 29.9611 9.70297 29.6769C10.1023 29.1017 10.5769 28.2571 10.7214 27.3063C10.8714 26.3201 10.6602 25.2339 9.71324 24.2869C8.76626 23.3399 7.68006 23.1287 6.69381 23.2787C5.74306 23.4233 4.89848 23.8979 4.32325 24.2972C4.03906 24.4945 3.70509 24.449 3.54299 24.2869L2.00043 22.7444C0.552147 21.2961 0.552147 18.9479 2.00043 17.4997L5.23992 14.2602L6.70522 15.7255C7.47196 16.4922 8.71509 16.4922 9.48183 15.7255C10.2486 14.9587 10.2486 13.7156 9.48183 12.9489L8.01653 11.4836L17.426 2.07404C18.8743 0.625755 21.2225 0.625756 22.6707 2.07404L24.2133 3.6166C24.3754 3.7787 24.4209 4.11267 24.2236 4.39686C23.8243 4.97209 23.3497 5.81667 23.2051 6.76742C23.0551 7.75367 23.2663 8.83986 24.2133 9.78685C25.1603 10.7338 26.2465 10.945 27.2327 10.7951C28.1835 10.6505 29.0281 10.1759 29.6033 9.77658C29.8875 9.5793 30.2214 9.62474 30.3836 9.78684L31.9261 11.3294C33.3744 12.7777 33.3744 15.1258 31.9261 16.5741L22.5166 25.9836ZM13.3382 16.8053C12.5715 16.0385 11.3284 16.0385 10.5616 16.8053C9.79488 17.572 9.79488 18.8151 10.5616 19.5819L14.418 23.4383C15.1848 24.205 16.4279 24.205 17.1946 23.4383C17.9614 22.6715 17.9614 21.4284 17.1946 20.6617L13.3382 16.8053Z'
+              fill='#192231'
+              stroke='#192231'
+              stroke-width='1.30891'
+            ></path>
+          </svg>
+          <span className='text-white	text-xl font-bold'>Обрати сеанс</span>
+        </Link>
         {/* Header */}
         <div
           className='movie-header relative min-h-[89.5vh] bg-cover bg-no-repeat bg-center bg-fixed'
@@ -69,7 +93,7 @@ const MoviePage = () => {
           <div className='w-full min-h-[89.5vh] bg-black/50'>
             <div className='absolute top-15 left-25 flex items-center'>
               <button
-                onClick={() => navigate("/")}
+                onClick={() => navigate('/')}
                 className='flex justify-center items-center w-13 h-13 mr-3 bg-white/20 hover:bg-white/50 cursor-pointer rounded-full transition-colors'
               >
                 <img src={searchIcon} alt='Search' className='w-5 h-5' />
@@ -188,7 +212,7 @@ const MoviePage = () => {
               src={movie.trailer}
               title='Трейлер'
               className='w-96 h-50 object-cover mb-2'
-              allowFullScreen              
+              allowFullScreen
             />
             <p className='font-bold text-lg'>Трейлер</p>
           </div>
@@ -300,10 +324,10 @@ const MoviePage = () => {
             </div>
           ) : (
             <div className='flex justify-center my-5 relative'>
-              <div className='absolute text-center lg:min-w-196 min-h-full z-10 bg-black/30 rounded-lg p-5 flex justify-center items-center text-3xl font-bold'>Авторизуйтеся, щоб мати змогу оцінити фільм.</div>
-              <form                
-                className=' filter blur-md flex flex-col gap-y-5 min-h-50 w-86 bg-[#1E293B] rounded-lg shadow-lg p-5 hover:scale-105 transition-all m-15'
-              >
+              <div className='absolute text-center lg:min-w-196 min-h-full z-10 bg-black/30 rounded-lg p-5 flex justify-center items-center text-3xl font-bold'>
+                Авторизуйтеся, щоб мати змогу оцінити фільм.
+              </div>
+              <form className=' filter blur-md flex flex-col gap-y-5 min-h-50 w-86 bg-[#1E293B] rounded-lg shadow-lg p-5 hover:scale-105 transition-all m-15'>
                 <label
                   htmlFor='rating'
                   className='text-center text-2xl font-bold'
@@ -320,7 +344,7 @@ const MoviePage = () => {
                   disabled
                   className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"'
                 />
-                <button                
+                <button
                   type='submit'
                   disabled
                   className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br hover:cursor-pointer focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
@@ -338,7 +362,10 @@ const MoviePage = () => {
                 </h3>
                 <div className='flex flex-wrap gap-3 px-16'>
                   {movie.ratings.map((rating, i) => (
-                    <div key={i} className='flex flex-col gap-y-3 min-h-36 min-w-36 bg-[#1E293B] rounded-lg shadow-lg p-5 hover:scale-105 transition-all'>
+                    <div
+                      key={i}
+                      className='flex flex-col gap-y-3 min-h-36 min-w-36 bg-[#1E293B] rounded-lg shadow-lg p-5 hover:scale-105 transition-all'
+                    >
                       <h4 className='text-xl text-center font-bold mb-2'>
                         {rating.user}
                       </h4>
@@ -363,6 +390,5 @@ const MoviePage = () => {
     </Navbar>
   );
 };
-
 
 export default MoviePage;
