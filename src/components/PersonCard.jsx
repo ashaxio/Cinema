338a738a5c2bc7b1f7ebdd221d97fc73b444286e
Oleksandr Bezverkhyi <bezverkhyi.ol@gmail.com>
2025-05-14 16:51:33 +0300

    Create a light theme for the MoviePage

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,22 @@
-const PersonCard = ({ name, photo, role }) => (
-  <div className='flex-shrink-0 w-60 bg-gray-800 rounded-lg p-4 text-center'>
-    <img
-      src={photo}
-      alt={name}
-      className='w-full h-60 object-cover rounded mb-3'
-    />
-    <h3 className='text-lg font-semibold'>{name}</h3>
-    <p className='text-sm text-white/70'>{role}</p>
-  </div>
-);
+import { useTheme } from "./ThemeContext";
+
+const PersonCard = ({ name, photo, role }) => {
+  const { isDarkMode } = useTheme();
+  return (
+    <div className="flex-shrink-0 w-60 bg-[var(--bg-movie-page-main)] rounded-lg p-4 text-center">
+      <img
+        src={photo}
+        alt={name}
+        className="w-full h-60 object-cover rounded mb-3"
+      />
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <p
+        className={`text-sm ${isDarkMode ? "text-white/70" : "text-black/70"}`}
+      >
+        {role}
+      </p>
+    </div>
+  );
+};
 
 export default PersonCard;
